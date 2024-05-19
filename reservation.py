e9c36de3f2a95e525b610284864182d923af6bc4
Xi Xu <i@xi-xu.me>
2024-05-19 13:45:44 +0800

    Update reservation.py

--- reservation.py
+++ reservation.py
@@ -17,34 +17,35 @@ import pyperclip as ppc
 import time
 from PIL import Image
 
-pag.click(x=1200, y=75)
+pag.click(x=1200, y=75)  # 地址栏坐标
 pag.typewrite("https://www.chaojibiaodan.com/form/1d1u2Nk8")
 pag.press("enter")
-while Image.fromarray(np.array(pag.screenshot(region=(1165, 35, 1, 1)))).getpixel(
-    (0, 0)
-) != (49, 193, 123):
+favicon_pos = (1165, 35)  # 网页图标坐标
+while Image.fromarray(
+    np.array(pag.screenshot(region=(favicon_pos[0], favicon_pos[1], 1, 1)))
+).getpixel((0, 0)) != (49, 193, 123):
     time.sleep(0.1)
-pag.click(x=1020, y=275)
+pag.click(x=1020, y=275)  # “学（工）号”输入框坐标
 pag.typewrite(学号)
-pag.click(x=1020, y=380)
+pag.click(x=1020, y=380)  # “姓名”输入框坐标
 ppc.copy(姓名)
 pag.hotkey("ctrl", "v")
-pag.click(x=1020, y=485)
+pag.click(x=1020, y=485)  # “学院”输入框坐标
 ppc.copy(学院)
 pag.hotkey("ctrl", "v")
-pag.click(x=1020, y=590)
+pag.click(x=1020, y=590)  # “手机号码”输入框坐标
 pag.typewrite(手机号码)
-pag.click(x=1020, y=735)
+pag.click(x=1020, y=735)  # “预订日期”输入框坐标
 pag.typewrite(预订日期)
 if 预订时段:
-    pag.click(x=1020, y=880)
+    pag.click(x=1020, y=880)  # “周一--周五时段”单选框坐标
     pag.scroll(-1000)
-    pag.click(x=1020, y=385)
-    pag.click(x=1020, y=445 + 25 * 周一至周五时段)
+    pag.click(x=1020, y=385)  # “周一至周五时段”菜单下拉按钮坐标
+    pag.click(x=1020, y=445 + 25 * 周一至周五时段)  # “周一至周五时段”菜单选项坐标
 else:
-    pag.click(x=1020, y=930)
+    pag.click(x=1020, y=930)  # “周六、周日时段”单选框坐标
     pag.scroll(-1000)
-    pag.click(x=1020, y=525)
-    pag.click(x=1020, y=590 + 25 * 周六周日时段)
-pag.click(x=1020, y=730)
-pag.click(x=1440, y=820)
+    pag.click(x=1020, y=525)  # “周六、周日时段”菜单下拉按钮坐标
+    pag.click(x=1020, y=590 + 25 * 周六周日时段)  # “周六、周日时段”菜单选项坐标
+pag.click(x=1020, y=730)  # “我已知晓查询方式”单选框坐标
+pag.click(x=1440, y=820)  # 提交按钮坐标
